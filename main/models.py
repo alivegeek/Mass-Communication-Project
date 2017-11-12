d60d2bd53c3ddbf69c40dd9ccf9d5c642c31010e
Nathan Holbrook <alivegeek@gmail.com>
2017-11-12 18:22:50 -0500

    begin work on adding models. First task is to capture and contacts and related data.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    firstName = models.CharField(max_length=25)
+    lastName = models.CharField(max_length=25)
+    phoneNumber = models.BigIntegerField()
+    email = models.EmailField(max_length=254)
+    note = models.TextField(max_length=512)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    last_updated = models.DateTimeField(
+            blank=True, null=True)
+    #
+    # def publish(self):
+    #     self.published_date = timezone.now()
+    #     self.save()
+    #
+    # def __str__(self):
+    #     return self.title
